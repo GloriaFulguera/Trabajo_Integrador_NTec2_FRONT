84a91c7276d56da828e86ee881d69e3a4793dd34
Gloria Fulguera <gloriafulguera@gmail.com>
2025-07-02 20:35:58 -0300

    agrego navbar y vista de solicitudes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SolicitudesComponent } from './Pages/solicitudes/solicitudes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:SolicitudesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
