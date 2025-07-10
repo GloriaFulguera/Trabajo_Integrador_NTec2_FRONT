8b39d1e6db8a9a616e2d4d5656da287ce581e792
Gloria Fulguera <gloriafulguera@gmail.com>
2025-07-10 02:04:12 -0300

    ajustes visuales segun accion, agrego btn aprobar y rechazar manuales

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginService } from '../../Services/login.service';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +8,26 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
-  constructor(private route:Router){}
+export class HomeComponent implements OnInit {
+  Dni = localStorage.getItem("user_dni");
+  Nombre: any;
+  Apellido: any;
+  Usuario: any;
+  constructor(private route: Router, private loginService: LoginService) { }
   ngOnInit(): void {
-    let logueado=localStorage.getItem("user_state");
-    if(!logueado){
+    let logueado = localStorage.getItem("user_state");
+    if (!logueado) {
       this.route.navigate(['/lock']);
     }
+    else{
+      this.GetUser();
+    }
+  }
+  GetUser() {
+    this.loginService.GetUser(this.Dni).subscribe(x => {
+      this.Usuario = x;
+      this.Nombre = this.Usuario[0].nombre;
+      this.Apellido = this.Usuario[0].apellido;
+    })
   }
 }
